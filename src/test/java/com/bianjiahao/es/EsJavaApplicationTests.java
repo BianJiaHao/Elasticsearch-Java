ff8ee02cdef90ac40699871b7b00367f45870efb
OrererereO <516807140@qq.com>
2022-03-03 16:32:57 +0800

    项目依赖的搭建

--- src/test/java/com/bianjiahao/es/EsJavaApplicationTests.java
+++ src/test/java/com/bianjiahao/es/EsJavaApplicationTests.java
@@ -1,13 +1,34 @@
 package com.bianjiahao.es;
 
+import com.alibaba.druid.pool.DruidDataSource;
+import org.apache.http.Header;
+import org.apache.http.HttpHost;
+import org.apache.http.message.BasicHeader;
+import org.elasticsearch.client.*;
+import org.elasticsearch.client.indices.GetIndexRequest;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @SpringBootTest
 class EsJavaApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @Autowired
+    DataSource dataSource;
+
+
+    public static void main(String[] args) throws IOException {
+        RestClientBuilder builder = RestClient.builder(new HttpHost("localhost", 9200, "http"));
+        RestClient client = builder.build();
+
+
+
     }
+
 
 }
