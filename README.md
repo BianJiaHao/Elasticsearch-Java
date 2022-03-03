8600bfbc6fa3f07dc05158e601c2689dbd0c6d98
OrererereO <516807140@qq.com>
2022-03-03 11:23:42 +0800

    commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Elasticsearch-Java
+
 Es java api
---
+++ src/main/java/com/bianjiahao/es/EsJavaApplication.java
@@ -0,0 +1,13 @@
+package com.bianjiahao.es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsJavaApplication.class, args);
+    }
+
+}
